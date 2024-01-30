76686216f4755210762f0d4e05465d49cc5d59da
Sophie Haber <SHaber@lcs.on.ca>
2024-01-30 14:48:47 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Sophie Haber on 30.01.24.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
